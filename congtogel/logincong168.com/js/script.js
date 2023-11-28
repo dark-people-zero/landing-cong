9d1f0e3d2d1821ffd8e585c2099a27d2a2ebad52
Dark People <dark.people.zero@gmail.com>
2023-11-29 00:53:25 +0700

    first commit

---
+++ congtogel/logincong168.com/js/script.js
@@ -0,0 +1,28 @@
+document.getElementById("link-1").onclick = function () {
+    window.location.href = "https://188.166.178.229:118/";
+  };
+  document.getElementById("link-2").onclick = function () {
+    window.location.href = "https://ayocong1.net/";
+  };
+  document.getElementById("link-3").onclick = function () {
+    window.location.href = "https://ayocong1.org/";
+  };
+  document.getElementById("link-4").onclick = function () {
+    window.location.href = "https://ayocong168.org/";
+  };
+  document.getElementById("link-5").onclick = function () {
+    window.location.href = "https://congbagicuan.com/";
+  };
+  document.getElementById("link-wap").onclick = function () {
+    window.location.href = "https://congbagicuan.net/lite/login.php";
+  };
+  document.getElementById("link-bonus").onclick = function () {
+    window.location.href = "http://promo01.cong168.org/";
+  };
+  document.getElementById("link-playstore").onclick = function () {
+    window.location.href =
+      "https://cong168.org/cdn/app/congtogel-v.4.7.apk";
+  };
+  document.getElementById("link-livechat").onclick = function () {
+    window.location.href = "https://direct.lc.chat/13407159/";
+  };
